ac6bd86a7d5f3d07034035beee01e1d8bb660a3b
Simon Beijer <beijersimon@gmail.com>
2020-04-29 00:16:46 +0200

    fixing router 8

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -32,7 +32,7 @@ class MainComponent extends React.Component<Props, State> {
           style={this.props.size === "desktop" ? MainStyle : MainStyleMobile}
         >
           <Switch>
-            <Route basename="/React-NASA-API-TypeScript">
+            <Route exact path="/">
               <Header
                 overflow={this.state.toggleSidebar}
                 size={this.props.size}
