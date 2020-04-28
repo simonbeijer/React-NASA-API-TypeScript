ea379f4148594fc23e8eeb8acf0f2a2519560589
Simon Beijer <beijersimon@gmail.com>
2020-04-29 00:10:40 +0200

    fixing router 6

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ class App extends React.Component<Props, State> {
   render() {
     return (
       <ErrorBoundary>
-        <BrowserRouter basename="/repoName">
+        <BrowserRouter basename="/React-NASA-API-TypeScript">
           <div className="App">
             <MainComponent size={this.state.size} />
           </div>
