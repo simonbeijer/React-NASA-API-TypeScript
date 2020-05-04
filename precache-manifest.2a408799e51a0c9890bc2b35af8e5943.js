774ebd4639d41837342dca468d9011f009ca6cca
Simon Beijer <beijersimon@gmail.com>
2020-05-04 18:27:20 +0200

    Updates

---
+++ precache-manifest.2a408799e51a0c9890bc2b35af8e5943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f88e992ecfb65ff8c79e5dee88e4f46",
+    "url": "/React-NASA-API-TypeScript/index.html"
+  },
+  {
+    "revision": "ed40f6fe472a152fd649",
+    "url": "/React-NASA-API-TypeScript/static/css/main.cc480219.chunk.css"
+  },
+  {
+    "revision": "5ecc7f352f6d8ba5a14f",
+    "url": "/React-NASA-API-TypeScript/static/js/2.251f85d5.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/React-NASA-API-TypeScript/static/js/2.251f85d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed40f6fe472a152fd649",
+    "url": "/React-NASA-API-TypeScript/static/js/main.cb486813.chunk.js"
+  },
+  {
+    "revision": "34ae08a1d85828145470",
+    "url": "/React-NASA-API-TypeScript/static/js/runtime-main.47fe0c7a.js"
+  }
+]);
